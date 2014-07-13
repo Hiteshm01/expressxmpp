3d6d92939c9aaf1d90c91caaeb74ba5c485bccd5
Hitesh <hitesh.mehta@paytm.com>
2014-07-13 23:58:36 +0530

    gcm integration2

---
+++ api/gcm.js
@@ -0,0 +1,9 @@
+var sender = new gcm.Sender('AIzaSyBcDCcYsu1bVrBviVSNONxOh01-ywbekO8');
+var registrationIds = [];
+registrationIds.push('APA91bFUVamBVdJ5zqu4kZjA2YbIIjz0rr-Kw-yN0BO3p0WEz4icL6jzNx9iycMwyLfCcB2VZZ2BIhhjGtZiq2jTDPk6GJYnHZgB59LJkHjLscnM0wut7RfHM41gcW4W0GOcYCHeZ4X3B3gWCQxkLL5j6Skvt4bmXQ');
+/**
+ * Params: message-literal, registrationIds-array, No. of retries, callback-function
+ **/
+sender.send(message, registrationIds, 4, function (err, result) {
+    console.log('gcm',err,result);
+});
