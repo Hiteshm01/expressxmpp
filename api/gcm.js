3fb189e71e044b8abf9e14e5a4d5b5756c8f4176
Hitesh <hitesh.mehta@paytm.com>
2014-07-14 00:02:01 +0530

    gcm integration4

--- api/gcm.js
+++ api/gcm.js
@@ -1,3 +1,4 @@
+var gcm = require('node-gcm');
 var sender = new gcm.Sender('AIzaSyBcDCcYsu1bVrBviVSNONxOh01-ywbekO8');
 var registrationIds = [];
 registrationIds.push('APA91bFUVamBVdJ5zqu4kZjA2YbIIjz0rr-Kw-yN0BO3p0WEz4icL6jzNx9iycMwyLfCcB2VZZ2BIhhjGtZiq2jTDPk6GJYnHZgB59LJkHjLscnM0wut7RfHM41gcW4W0GOcYCHeZ4X3B3gWCQxkLL5j6Skvt4bmXQ');
@@ -8,4 +9,4 @@ sender.send(message, registrationIds, 4, function (err, result) {
     console.log('gcm',err,result);
 });
 
-module.exports = gcm;
+module.exports = sender;
