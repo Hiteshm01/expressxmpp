4b92402abe0b2d878431ff0c99183549b3ec3a03
Hitesh <hitesh.mehta@paytm.com>
2014-07-13 12:09:36 +0530

    parrot server

--- api/router.js
+++ api/router.js
@@ -1,14 +1,30 @@
 var debug = require('debug')('router');
+var debugManager = require('debug')('routerManager');
+var routerManager = {
+	registerRoute = function(jid, session){
+		debugManager('registerRoute', jid);
+	    this.sessions[jid] = client;
+	    return true;
+	}
+	removeRoute = function(jid){
+		debugManager('registerRoute', jid);
+	    this.sessions[jid] = {};
+	    return true;
+	}
+}
 function router(server) {
 	server.on('connect', function (client) {
 		client.on('online', function () {
 			debug('online!');
+			routerManager.registerRoute(client.jid.local,client);
 		});
 		client.on('end', function () {
+			routerManager.removeRoute(client.jid.local);
 			debug('end');
 		});
 		client.on('stanza', function (stanza) {
 			debug('stanza');
+			this.sessions[client.jid.local].send(stanza); 
 		});
 	});
 }
