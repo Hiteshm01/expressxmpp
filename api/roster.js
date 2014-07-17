f9f208b172e8bd6edd6254cd57ebe3190b214bd1
Hitesh <hitesh.mehta@paytm.com>
2014-07-18 00:15:05 +0530

    roster fix7

--- api/roster.js
+++ api/roster.js
@@ -10,30 +10,32 @@ function roster(server) {
 
 		client.on('stanza', function (stanza) {
 			debug('stanza');
-			if (stanza.attrs.type === "get") {
-				stanza.attrs.type = "result";
-				var query = null;
-				redis.findRandomUser(stanza.attrs.from, function (err, res) {
-					debug('Assigning random user with jid: ', res);
-					stanza.attrs.to = stanza.attrs.from;
-					query.c("item", {
-						jid: res,
-						name: 'stranger',
-						// subscription: item.state
+			if (stanza.is('iq') && (query = stanza.getChild('query', "jabber:iq:roster"))) {
+				if (stanza.attrs.type === "get") {
+					stanza.attrs.type = "result";
+					var query = null;
+					redis.findRandomUser(stanza.attrs.from, function (err, res) {
+						debug('Assigning random user with jid: ', res);
+						stanza.attrs.to = stanza.attrs.from;
+						query.c("item", {
+							jid: res,
+							name: 'stranger',
+							// subscription: item.state
+						});
+						client.send(stanza);
 					});
-					client.send(stanza);
-				});
-				// RosterStorage.find(new xmpp.JID(stanza.attrs.from).bare().toString(), function (roster) {
-				// 	roster.items.forEach(function (item) {
-				// 		query.c("item", {
-				// 			jid: item.jid,
-				// 			name: item.name,
-				// 			subscription: item.state
-				// 		});
-				// 	});
-				// 	stanza.attrs.to = stanza.attrs.from;
-				// 	client.send(stanza);
-				// });
+					// RosterStorage.find(new xmpp.JID(stanza.attrs.from).bare().toString(), function (roster) {
+					// 	roster.items.forEach(function (item) {
+					// 		query.c("item", {
+					// 			jid: item.jid,
+					// 			name: item.name,
+					// 			subscription: item.state
+					// 		});
+					// 	});
+					// 	stanza.attrs.to = stanza.attrs.from;
+					// 	client.send(stanza);
+					// });
+				}
 			}
 		});
 	});
