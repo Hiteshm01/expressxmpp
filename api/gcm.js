4505d787c23238ec0c103d063ae6d207a5c96fed
Hitesh <hitesh.mehta@paytm.com>
2014-07-17 23:29:45 +0530

    canonical fix3

--- api/gcm.js
+++ api/gcm.js
@@ -2,6 +2,7 @@ var gcm = require('node-gcm');
 var debug = require('debug')('gcm');
 var sender = new gcm.Sender('AIzaSyBcDCcYsu1bVrBviVSNONxOh01-ywbekO8');
 var registrationIds = [];
+var db = require("../model").db;
 registrationIds.push('APA91bFUVamBVdJ5zqu4kZjA2YbIIjz0rr-Kw-yN0BO3p0WEz4icL6jzNx9iycMwyLfCcB2VZZ2BIhhjGtZiq2jTDPk6GJYnHZgB59LJkHjLscnM0wut7RfHM41gcW4W0GOcYCHeZ4X3B3gWCQxkLL5j6Skvt4bmXQ');
 /**
  * Params: message-literal, registrationIds-array, No. of retries, callback-function
